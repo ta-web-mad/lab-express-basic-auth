de0a7907ca4953277322cac775d1979b016b421a
Teo-Lopez <emaildeteo@gmail.com>
2021-04-16 13:06:50 +0200

    modified boilerplate

--- app.js
+++ app.js
@@ -1,37 +1,28 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
+// DB connection
+require('./config/db.config')
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+// Debug
+require('./config/debug.config')
 
-const app = express();
+// HBS
+require('./config/hbs.config')
 
-// require database configuration
-require('./configs/db.config');
+// App
+const express = require('express')
+const app = express()
 
-// Middleware Setup
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// App settings
+require('./config/sass.config')(app)
+require('./config/middleware.config')(app)
+require('./config/views.config')(app)
+require('./config/locals.config')(app)
 
-// Express View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+// Routes index
+require('./routes')(app)
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+// Error handling
+require('./config/error-handlers.config')(app)
 
-const index = require('./routes/index.routes');
-app.use('/', index);
-
-module.exports = app;
+module.exports = app
